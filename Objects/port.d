ab3a755952d29708fa536812ab1e92290b78e23c
Alakey <Salamakhin67@gmail.com>
2022-03-13 14:20:30 +0300

    init commit

---
+++ Objects/port.d
@@ -0,0 +1,15 @@
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\portable\RVDS\ARM_CM4F\port.c
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\FreeRTOS.h
+.\objects\port.o: S:\Programs\keil\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\port.o: S:\Programs\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\port.o: .\RTE\RTOS\FreeRTOSConfig.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h
+.\objects\port.o: S:\Programs\keil\ARM\ARMCC\Bin\..\include\string.h
+.\objects\port.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\projdefs.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\portable.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\deprecated_definitions.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\portable\RVDS\ARM_CM4F\portmacro.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\mpu_wrappers.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\task.h
+.\objects\port.o: S:\Programs\keil_pack\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\list.h
---
+++ main.c
@@ -0,0 +1,55 @@
+#include "stm32f4xx.h"                  // Device header
+#include "FreeRTOS.h"                   // ARM.FreeRTOS::RTOS:Core
+#include "FreeRTOSConfig.h"             // ARM.FreeRTOS::RTOS:Config
+#include "task.h"                       // ARM.FreeRTOS::RTOS:Core
+#include "queue.h"                      // ARM.FreeRTOS::RTOS:Core
+#include "inttypes.h"
+
+
+void vBlinkTaskFunction(void* pvParameters)
+{
+	while(1)
+	{
+		GPIOA->ODR |= GPIO_ODR_ODR_5; // led on A5
+		vTaskDelay(1000);
+		GPIOA->ODR &= ~GPIO_ODR_ODR_5; //led off
+		vTaskDelay(1000);
+	}
+}
+
+void vRunningTaskFunction(void* pvParameters)
+{
+	while(1)
+	{
+		for (int i=0;i<8;i++)
+		{
+			GPIOC->ODR = (1 << (15-i)) | (1 << (i));
+			vTaskDelay(2000);
+		}
+	}
+}
+
+//c7-c12
+void InitHardware()
+{
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;  //razreshenie raboti ustroistv
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0; //00 - input, 01 - out, 10 - alt func, 11 - analog input
+}
+
+
+int main ()
+{
+
+	InitHardware();
+	
+	xTaskCreate(vBlinkTaskFunction, "blink", configMINIMAL_STACK_SIZE, NULL, 4, NULL);
+	xTaskCreate(vRunningTaskFunction, "runnig", configMINIMAL_STACK_SIZE, NULL, 4, NULL);
+	vTaskStartScheduler();
+
+	
+	while(1)
+	{
+
+	}
+}
