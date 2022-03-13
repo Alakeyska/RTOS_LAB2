413b3968dd38dbc07da4c59bf0744ca7d24b32d4
Alakey <Salamakhin67@gmail.com>
2022-03-13 15:29:28 +0300

    punct9

--- main.c
+++ main.c
@@ -4,23 +4,45 @@
 #include "task.h"                       // ARM.FreeRTOS::RTOS:Core
 #include "queue.h"                      // ARM.FreeRTOS::RTOS:Core
 #include "inttypes.h"
-#include <time.h>
+
+
+void InitHardware();
+void vBlinkTaskFunction();
+void vRunningTaskFunction();
+TaskHandle_t blink, running;
+
+
+int main ()
+{
+	InitHardware();
+	
+	xTaskCreate(vBlinkTaskFunction, "blink", configMINIMAL_STACK_SIZE, NULL, 4, &blink);
+	xTaskCreate(vRunningTaskFunction, "runnig", configMINIMAL_STACK_SIZE, NULL, 8, &running);
+	vTaskStartScheduler();
+	while(1)
+	{
+
+	}
+}
 
 
 void vBlinkTaskFunction(void* pvParameters)
 {
 	while(1)
 	{
-		for (int m=0;m<4;m++)
+		vTaskSuspend(running);
+		for (int m=0;m<8;m++)
 		{
 			GPIOA->ODR |= GPIO_ODR_ODR_5; // led on A5
 			vTaskDelay(1000);
 			GPIOA->ODR &= ~GPIO_ODR_ODR_5; //led off
 			vTaskDelay(1000);
 		}
-		vTaskDelay(100);
+		vTaskResume(running);
+		vTaskDelay(2000);
 	}
 }
+
 
 void vRunningTaskFunction(void* pvParameters)
 {
@@ -38,27 +60,10 @@ void vRunningTaskFunction(void* pvParameters)
 	}
 }
 
-//c7-c12
+
 void InitHardware()
 {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;  //razreshenie raboti ustroistv
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0; //00 - input, 01 - out, 10 - alt func, 11 - analog input
-}
-
-
-int main ()
-{
-
-	InitHardware();
-	
-	xTaskCreate(vBlinkTaskFunction, "blink", configMINIMAL_STACK_SIZE, NULL, 4, NULL);
-	xTaskCreate(vRunningTaskFunction, "runnig", configMINIMAL_STACK_SIZE, NULL, 8, NULL);
-	vTaskStartScheduler();
-
-	
-	while(1)
-	{
-
-	}
 }
